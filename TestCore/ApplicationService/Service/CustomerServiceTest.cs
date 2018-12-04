7d81faf7eb1db47e12e232a78f37f9dfe1a48439
15488 <mads.gadeberg@nordakademie.de>
2018-12-04 13:30:37 +0100

    Test of reading all customers calling repo

--- TestCore/ApplicationService/Service/CustomerServiceTest.cs
+++ TestCore/ApplicationService/Service/CustomerServiceTest.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using Droneshop.Core.ApplicationService;
+using Droneshop.Core.ApplicationService.Services;
+using Droneshop.Core.DomainService;
+using Droneshop.Core.Entity;
+using Moq;
+using Xunit;
+
 namespace TestCore
 {
     public class CustomerServiceTest
     {
-         
+        [Fact]
+        public void ReadAllCustomersEnsureRepositoryIsCalled()
+        {
+            var customerRepo = new Mock<ICustomerRepository>();
+            ICustomerService customerService = new CustomerService(customerRepo.Object);
+
+            var isCalled = false;
+
+            customerRepo.Setup(x => x.ReadAllCustomers()).Callback(() => isCalled = true).Returns(new List<Customer>());
+            
+            Assert.True(isCalled);
+        }
     }
 }
