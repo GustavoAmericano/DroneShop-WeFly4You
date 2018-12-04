ae274161255c5982729d72c09de2c11992b706d9
fjt1509 <fjt1509@hotmail.com>
2018-12-04 09:16:02 +0100

    travis update

--- Droneshop.Core/ApplicationService/IUserService.cs
+++ Droneshop.Core/ApplicationService/IUserService.cs
@@ -10,5 +10,6 @@ namespace Droneshop.Core.ApplicationService
         User CreateUser(User user);
         User UpdateUser(User user);
         User DeleteUser(int id);
+        //test git
     }
 }
